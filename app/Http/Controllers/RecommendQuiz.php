32ff1d8af04205b787bcd59fa73abeaadf45b002
ahmed magdy <a.magdi65@gamil.com>
2019-04-29 22:15:41 +0200

    added function get all user and get quizzes for reccommend quiz

---
+++ app/Http/Controllers/RecommendQuiz.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RecommendQuiz extends Controller
+{
+    public function GetAllUsers()
+    {
+        $response=array(
+        array(
+                'id'=>0,
+                'username'=>'ahmed magdy',
+                'age' => 20,
+                'interests' =>'software',
+                'score' => 25
+        ),array(
+            'id'=>1,
+            'username'=>'ahmed zizo',
+            'age' => 20,
+            'interests' =>'worker',
+            'score' => 50
+        ),array(
+        'id'=>3,
+        'username'=>'ane magdy',
+        'age' => 20,
+        'interests' =>'software',
+        'score' => 10
+        ));
+        return json_encode($response);
+    }
+    public function getAllQuizess()
+    {
+        $response=array(
+            array(
+                'title'=>"java quiz",
+                'pass_score'=>10,
+                'skill_type' =>array("name"=>'software')
+            ),array(
+                'title'=>"building quiz",
+                'pass_score'=>12,
+                'skill_type' =>array("name"=>'worker')
+            ),array(
+                'title'=>"c++ quiz",
+                'pass_score'=>15,
+                'skill_type' =>array("name"=>'software')
+            ));
+            return json_encode($response);
+    }
+
+    
+}
